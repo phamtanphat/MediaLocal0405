ce698214148e746edbfba05917f4066c7a5f8461
thuviengithub0208 <khoaphp@yahoo.com>
2018-07-06 17:51:14 +0700

    "Anh xa"

--- app/src/main/java/com/ptp/phamtanphat/medialocal0405/MainActivity.java
+++ app/src/main/java/com/ptp/phamtanphat/medialocal0405/MainActivity.java
@@ -2,12 +2,21 @@ package com.ptp.phamtanphat.medialocal0405;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnPlayMp3,btnPlayMp4;
+    VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnPlayMp3 = findViewById(R.id.buttonPlayMp3);
+        btnPlayMp4 = findViewById(R.id.buttonPlayMp4);
+        videoView = findViewById(R.id.videoview);
     }
 }
